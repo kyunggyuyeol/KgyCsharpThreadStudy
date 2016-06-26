eaf20f167dcb8c5a4578521b2a0bccb8e8b4c68d
KGY <KGY@DESKTOP-CGUKMCE>
2016-06-27 08:21:29 +0900

    06/27 스레드에 필요한 파라메터 전달 방법

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,35 +12,71 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
-            ThreadSample Foreground = new ThreadSample(10);//객체 생성시 값 받아 쓰려면 이렇게 사용해야한다.
-            ThreadSample background = new ThreadSample(20);
+            ThreadSample sample = new ThreadSample(10);
 
-            Thread ThreadOne = new Thread(Foreground.CountNumbers);
-            ThreadOne.Name = "Foreground";
-            Thread ThreadTwo = new Thread(background.CountNumbers);
-            ThreadTwo.Name = "Background";
-            ThreadTwo.IsBackground = true;
-
+            Thread ThreadOne = new Thread(sample.CountNumbers);
+            ThreadOne.Name = "threadOne";
             ThreadOne.Start();
-            ThreadTwo.Start();
+            ThreadOne.Join();
+            Console.WriteLine("------------------------------------------");
+
+            Thread ThreadTwo = new Thread(Count);
+            ThreadTwo.Name = "ThreadTwo";
+            ThreadTwo.Start(8);
+            ThreadTwo.Join();
+            Console.WriteLine("------------------------------------------");
+
+            Thread ThreadThree = new Thread(() => CountNumbers(12));
+            ThreadThree.Name = "ThreadThree";
+            ThreadThree.Start();
+            ThreadThree.Join();
+            Console.WriteLine("------------------------------------------");
+
+            int i = 10;
+            Thread ThreadFour = new Thread(() => PrintNumber(i));
+            i = 20;
+            Thread ThreadFive = new Thread(() => PrintNumber(i));
+            ThreadFour.Start();
+            ThreadFive.Start();
+
         }
-    }
 
-    class ThreadSample
-    {
-        private readonly int _iterations;//읽기전용
-
-        public ThreadSample(int _iterations)// 클래스와 같은 이름으로 만듬
+        static void Count(object value)
         {
-            this._iterations = _iterations;
+            CountNumbers((int)value);
         }
 
-        public void CountNumbers()
+        static void CountNumbers(int value)
         {
-            for (int i = 0; i < _iterations; i++)
+            for (int i = 0; i < value; i++)
             {
                 Thread.Sleep(TimeSpan.FromSeconds(0.5));
-                Console.WriteLine("{0} prints {1}", Thread.CurrentThread.Name, i);
+                Console.WriteLine("{0} Prints {1}", Thread.CurrentThread.Name, i);
+            }
+        }
+
+        static void PrintNumber(int number)
+        {
+            Console.WriteLine(number);
+        }
+
+        class ThreadSample
+        {
+            private readonly int _value;
+
+            public ThreadSample(int value)
+            {
+                this._value = value;
+            }
+
+
+            public void CountNumbers()
+            {
+                for (int i = 0; i < _value; i++)
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(0.5));
+                    Console.WriteLine("{0} prints {1}", Thread.CurrentThread.Name, i);
+                }
             }
         }
     }
