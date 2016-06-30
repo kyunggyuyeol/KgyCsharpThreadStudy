9f3df4c94fc6a9ea66a921acfb6954e7fe159855
KGY <KGY@DESKTOP-CGUKMCE>
2016-07-01 08:16:11 +0900

    7/1 part2 전 clear commit

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,41 +12,6 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
-            Thread t = new Thread(FaultyThread);
-            t.Start();
-            t.Join();
-
-            Console.WriteLine("--------------------------------------------------");
-            try
-            {
-                t = new Thread(badFaultyThread);
-                t.Start();
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine("We wont't get here!");
-            }
-        }
-
-        static void badFaultyThread()
-        {
-            Console.WriteLine("Starting a faulty thread...");
-            Thread.Sleep(2000);
-            throw new Exception("Boom!");
-        }
-
-        static void FaultyThread()
-        {
-            try
-            {
-                Console.WriteLine("Starting a fault thread");
-                Thread.Sleep(1000);
-                throw new Exception("Boom!");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Exception Handled: {0}", ex.Message);
-            }
         }
     }
 }
