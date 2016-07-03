95fc51255acf0f1e9104fcbd2657cd9dcc796781
경규열 <p82468@paran.com>
2016-07-03 20:26:14 +0900

    7/3 뮤텍스 스레드 동기화 기법중 하나 네트워크의 토큰링기법과 같음

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,88 +12,21 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Incorrect Counter");
+            const string MutexName = "CsharpThreadingCookBook";
 
-            Counter c = new Counter();
-
-            Thread t1 = new Thread(() => TestCounter(c));
-            Thread t2 = new Thread(() => TestCounter(c));
-            Thread t3 = new Thread(() => TestCounter(c));
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            t1.Join();
-            t2.Join();
-            t3.Join();
-
-            Console.WriteLine("TotalCount :{0}", c.count);
-            Console.WriteLine("--------------------------------------------------");
-
-            Console.WriteLine("DCorrect counter");
-
-            counterNoLock c1 = new counterNoLock();
-
-            t1 = new Thread(() => TestCounter(c1));
-            t2 = new Thread(() => TestCounter(c1));
-            t3 = new Thread(() => TestCounter(c1));
-            t1.Start();
-            t2.Start();
-            t3.Start();
-            t1.Join();
-            t2.Join();
-            t3.Join();
-            Console.WriteLine("TotalCount :{0}", c1.Count);
-        }
-
-        static void TestCounter(CounterBase c)
-        {
-            for (int i = 0; i < 100000; i++)
+            using (Mutex m = new Mutex(false, MutexName))
             {
-                c.Increment();
-                c.Decrement();
+                if(m.WaitOne(TimeSpan.FromSeconds(5),false))
+                {
+                    Console.WriteLine("Second instance is running!");
+                }
+                else
+                {
+                    Console.WriteLine("Running");
+                    Console.ReadLine();
+                    m.ReleaseMutex();
+                }
             }
-        }
-
-        class Counter : CounterBase
-        {
-            private int _count;
-
-            public int count
-            {
-                get { return _count; }
-            }
-
-            public override void Increment()
-            {
-                _count++;
-            }
-            public override void Decrement()
-            {
-                _count--;
-            }
-        }
-
-        class counterNoLock : CounterBase
-        {
-            private int _count;
-
-            public int Count { get { return _count; } }
-
-            public override void Increment()
-            {
-                Interlocked.Increment(ref _count);
-            }
-
-            public override void Decrement()
-            { 
-                Interlocked.Decrement(ref _count);
-            }
-        }
-
-        abstract class CounterBase
-        {
-            public abstract void Increment();
-            public abstract void Decrement();
         }
     }
 }
