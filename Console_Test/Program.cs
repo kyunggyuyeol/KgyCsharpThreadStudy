5629c075f94f5068da24b8298b73a5849702d5cc
KGY <KGY@DESKTOP-CGUKMCE>
2016-07-04 17:51:19 +0900

    7/4 Clear Commit

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,26 +12,6 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i < 7; i++)
-            {
-                string threadName = "thread : " + i.ToString();
-                int secondsToWait = 2 + 2 * i;
-
-                Thread t = new Thread(() => AccessDatabase(threadName, secondsToWait));
-                t.Start(); 
-            }
-        }
-
-        static SemaphoreSlim _semaphore = new SemaphoreSlim(4); // 동시스레드 갯수 지정
-
-        static void AccessDatabase(string Name, int seconds)
-        {
-            Console.WriteLine("{0} waits to access a database", Name);
-            _semaphore.Wait();
-            Console.WriteLine("{0} was granted an access to a databses", Name);
-            Thread.Sleep(TimeSpan.FromSeconds(seconds));
-            Console.WriteLine("{0} is completed", Name);
-            _semaphore.Release();
         }
     }
 }
