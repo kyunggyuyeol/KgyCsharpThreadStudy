56e39ebed26c6f5362e847d9621312e8abda491b
KGY <KGY@DESKTOP-CGUKMCE>
2016-07-06 15:59:57 +0900

    7/6 CountdownEvent 생성시 카운트를 만들어두고 카운트 만큼 스레드를 만들어 작업한다.  나에게 있어 가장 유용한 방법일것 같다

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -10,39 +10,26 @@ namespace Console_Test
 {
     class Program
     {
-        static void Main(string[] args) //ManualResetEventSlim 생성자 사용
+        static void Main(string[] args) //CountdownEvent 사용
         {
-            Thread v1 = new Thread(() => TravelTrouhGates("Thread1", 5));
-            Thread v2 = new Thread(() => TravelTrouhGates("Thread2", 6));
-            Thread v3 = new Thread(() => TravelTrouhGates("Thread3", 12));
-            v1.Start();
-            v2.Start();
-            v3.Start();
+            Console.WriteLine("Starting two operations");
+            Thread t1 = new Thread(() => PerformOperation("operation 1 is Completed", 4));
+            Thread t2 = new Thread(() => PerformOperation("operation 2 is Completed", 8));
 
-            Thread.Sleep(TimeSpan.FromSeconds(6));
-            Console.WriteLine("Thre gates are now open!");
-            _mainEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            _mainEvent.Reset();
-            Console.WriteLine("The gates have been closed");
-            Thread.Sleep(TimeSpan.FromSeconds(10));
-            Console.WriteLine("Thre gates are now open for the second time!");
-            _mainEvent.Set();
-            Thread.Sleep(TimeSpan.FromSeconds(2));
-            Console.WriteLine("Thre gates have been closed!");
-            _mainEvent.Reset();
+            t1.Start();
+            t2.Start();
+            _countdown.Wait();
+            Console.WriteLine("Both operaions have been completed.");
+            _countdown.Dispose();
         }
 
-        static ManualResetEventSlim _mainEvent = new ManualResetEventSlim(false);
+        static CountdownEvent _countdown = new CountdownEvent(2);
 
-        static void TravelTrouhGates(string threadName, int seconds)
+        static void PerformOperation(string message, int seconds)
         {
-            Console.WriteLine("{0} falls to sleep", threadName);
             Thread.Sleep(TimeSpan.FromSeconds(seconds));
-
-            Console.WriteLine("{0} waits for the gates to Open!", threadName);
-            _mainEvent.Wait();
-            Console.WriteLine("{0} enters the gates!", threadName);
+            Console.WriteLine(message);
+            _countdown.Signal();
         }
     }
 }
