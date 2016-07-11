fae62ffbb1bd4f73fe9a6c7a86e358e3a64affcd
KGY <KGY@DESKTOP-CGUKMCE>
2016-07-11 09:25:27 +0900

    7/11 volatile 키워드 여러 스레드가 동시에 필드를 수정할수도 있는 필드이다. 컴파일러가 제한하지 않음

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,7 +12,44 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
+            Thread t1 = new Thread(UserModeWait);
+            Thread t2 = new Thread(HybridSpinWait);
 
+            Console.WriteLine("Running user mode waiting");
+            t1.Start();
+            Thread.Sleep(20);
+            _isCompleted = true;
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            _isCompleted = false;
+            Console.WriteLine("Running hybrid SpinWait construct waiting");
+            t2.Start();
+            Thread.Sleep(5);
+            _isCompleted = true;
         }
+
+        static volatile bool _isCompleted = false;
+        
+        static void UserModeWait()
+        {
+            while(!_isCompleted)
+            {
+                Console.Write(".");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Waiting is complete");
+        }
+
+        static void HybridSpinWait()
+        {
+            SpinWait w = new SpinWait();
+            while(!_isCompleted)
+            {
+                w.SpinOnce();
+                Console.WriteLine(w.NextSpinWillYield);
+            }
+            Console.WriteLine("Waition is complete");
+        }
+
+        
     }
 }
