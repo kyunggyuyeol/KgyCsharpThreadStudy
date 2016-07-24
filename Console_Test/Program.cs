0a7f178294c0ed585e661ddcb1791998faf55bee
KGY <KGY@DESKTOP-CGUKMCE>
2016-07-25 08:52:52 +0900

    7/25 thread pool queue에 넣어 작업을 실행시킨다

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,6 +12,37 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
+            const int x = 1;
+            const int y = 2;
+            const string lambadaState = "lambada state 2";
+
+            ThreadPool.QueueUserWorkItem(AsyncOperation);
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(AsyncOperation, "async state");
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+
+            ThreadPool.QueueUserWorkItem(state =>
+            {
+                Console.WriteLine("Operation state: {0}", state);
+                Console.WriteLine("Worker thread id:{0}", Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Console.WriteLine("operation state: {0}, {1}", x + y, lambadaState); Console.WriteLine("Worker thread id: {0}", Thread.CurrentThread.ManagedThreadId);
+                Thread.Sleep(TimeSpan.FromSeconds(2));
+            }, "lambda state");
+            Thread.Sleep(TimeSpan.FromSeconds(2));
         }
+
+        private static void AsyncOperation(object state)
+        {
+            Console.WriteLine("Operation state: {0}", state ?? "(null)");
+            Console.WriteLine("Worker thread id:{0}", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+        }
+
     }
 }
