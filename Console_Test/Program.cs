51701e8f4f6aa043451b7197b87b28b83cdea1f5
경규열 <p82468@paran.com>
2016-08-02 08:26:57 +0900

    8/2 clear commit

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,58 +12,6 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {
-            const int numberOfOperations = 500;
-
-            var sw = new Stopwatch();
-            sw.Start();
-            UseThreads(numberOfOperations);
-            sw.Stop();
-            Console.WriteLine("execution time using Thread:{0}", sw.ElapsedMilliseconds);
-            sw.Reset();
-
-            sw.Start();
-            UseThreadPool(numberOfOperations);
-            sw.Stop();
-            Console.WriteLine("execution time using Thread:{0}", sw.ElapsedMilliseconds);
-        }
-
-        static void UseThreads(int numberOfOperations)
-        {
-            using (var countdown = new CountdownEvent(numberOfOperations))
-            {
-                Console.WriteLine("scheduling work by creating threads");
-                for (int i = 0; i < numberOfOperations; i++)
-                {
-                    var thread = new Thread(() =>
-                    {
-                        Console.WriteLine("{0}", Thread.CurrentThread.ManagedThreadId);
-                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
-                        countdown.Signal();
-                    });
-                    thread.Start();
-                }
-                countdown.Wait();
-                Console.WriteLine();
-            }
-        }
-
-        static void UseThreadPool(int numberOfOperations)
-        {
-            using (var countdown = new CountdownEvent(numberOfOperations))
-            {
-                Console.WriteLine("Staring work on a threadpool");
-                for (int i = 0; i < numberOfOperations; i++)
-                {
-                    ThreadPool.QueueUserWorkItem(_ =>
-                    {
-                        Console.WriteLine("{0}", Thread.CurrentThread.ManagedThreadId);
-                        Thread.Sleep(TimeSpan.FromSeconds(0.1));
-                        countdown.Signal();
-                    });
-                }
-                countdown.Wait();
-                Console.WriteLine();
-            }
         }
     }
 }
