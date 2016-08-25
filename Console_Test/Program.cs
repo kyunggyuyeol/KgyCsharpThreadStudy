b52abd5cad89dea3ba4d510c11316d990677f621
KGY_PC <KGY_PC@KGY_PC>
2016-08-25 17:30:17 +0900

    8/25 Task로 기본적인 연산수행

--- Console_Test/Program.cs
+++ Console_Test/Program.cs
@@ -12,21 +12,44 @@ namespace Console_Test
     {
         static void Main(string[] args)
         {//태스크 사용 Study
-            Task t1 = new Task(() => TaskMethod("TASK 1"));
-            Task t2 = new Task(() => TaskMethod("TASK 2"));
-            t2.Start();
-            t1.Start();
+            TaskMethod("Main Thread Task"); //thread pool 에서 실행 안함
 
-            Task.Run(() => TaskMethod("TASK 3"));
-            Task.Factory.StartNew(() => TaskMethod("TASK 4"));
-            Task.Factory.StartNew(() => TaskMethod("TASK 5"), TaskCreationOptions.LongRunning);
-            Thread.Sleep(TimeSpan.FromSeconds(1));
+            Task<int> task = CreateTask("Task 1"); //thread pool에 배치
+            task.Start();
+            int result = task.Result;
+            Console.WriteLine("Result is: {0}", result);
+
+            task = CreateTask("Task 2");
+            task.RunSynchronously(); //task 옵션에 의해 메인스레드에서 실행
+            result = task.Result;
+            Console.WriteLine("Result is: {0}", result);
+
+            task = CreateTask("Task 3");
+            task.Start();
+
+            while(!task.IsCompleted) //task1 과 동일하지만 task3가 끝날떄까지 상태를 출력한다.
+            {
+                Console.WriteLine(task.Status);
+                Thread.Sleep(TimeSpan.FromSeconds(0.5));
+            }
+
+            Console.WriteLine(task.Status);
+            result = task.Result;
+            Console.WriteLine("Result is : {0}", result);
         }
 
-        static void TaskMethod(string Name)
+        static Task<int> CreateTask(string name)
         {
-            Console.WriteLine("Task {0} is running on a thread id {1}. Is thread pool thread :{2}", Name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
-
+            return new Task<int>(() => TaskMethod(name));
         }
+
+        static int TaskMethod(string name)
+        {
+            Console.WriteLine("Task {0} is running on a thread id {1}. Is thread pool thread: {2}", name, Thread.CurrentThread.ManagedThreadId, Thread.CurrentThread.IsThreadPoolThread);
+            Thread.Sleep(TimeSpan.FromSeconds(2));
+            return 42;
+        }
+
+
     }
 }
